9a7067b077410fff0f11b31af9afe9dd43f5835b
Brent Champion <brent.champion@queensu.ca>
2020-09-15 02:26:19 -0400

    removing front-end warnings

--- client/src/components/pages/party/SharingURL.js
+++ client/src/components/pages/party/SharingURL.js
@@ -1,28 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
-import { blue } from '@material-ui/core/colors';
-
-const emails = ['username@gmail.com', 'user02@gmail.com'];
-const useStyles = makeStyles({
-  avatar: {
-    backgroundColor: blue[100],
-    color: blue[600],
-  },
-});
 
 export default function SharingURLWindow(props) {
-  const classes = useStyles();
   const { onClose, selectedValue, open } = props;
 
   const handleClose = () => {
     onClose(selectedValue);
-  };
-
-  const handleListItemClick = (value) => {
-    onClose(value);
   };
 
   return (
