eb3cae3dc3ad74016261d317a6dd635e514171e3
Brent Champion <brent.champion@queensu.ca>
2020-09-15 01:46:23 -0400

    updates styling for party page

--- client/src/components/pages/party/Party.js
+++ client/src/components/pages/party/Party.js
@@ -1,15 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import Backdrop from '@material-ui/core/Backdrop';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import api from '../../../api';
 import qs from "query-string";
-import { Input, TextField } from '@material-ui/core';
+import { TextField } from '@material-ui/core';
 
-const Party = (props) => {
+const useStyles = makeStyles({
+    root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+        textTransform: 'capitalize',
+    },
+    backdrop: {
+        zIndex: 100 + 1,
+        color: '#fff',
+      },
+});
+
+export default function Party(props) {
+    const classes = useStyles();
     const [room, setRoom] = useState(null);
     const [textOpen, setTextOpen] = useState(false);
     const [name, setName] = useState('');
+    const [open, setOpen] = useState(false);
 
+    /* Component Life Cycle Methods */
     useEffect(() => {
         console.log("Rendering component");
         const query = qs.parse(props.location.search);
@@ -19,11 +43,15 @@ const Party = (props) => {
         }
     }); // Add any listeners we want here 
 
+
     const generatePartyRoom = async () => {
         console.log("Generating new party room with name: " + name);
+        setOpen(true);
         const res = await api.party.newRoom(name); // Will need to pass in user token here ?
         // Add some shit here for catching bad API calls 
         setRoom(name);
+        setOpen(false);
+        setTextOpen(false);
         console.log(res);
     }
 
@@ -33,20 +61,20 @@ const Party = (props) => {
         setRoom(res.name);
     }
 
-    const generateSharingLink = (id) => {
-
-    }
-
     const onChangeName = e => {
         setName(e.target.value);
     }
 
     return (
         <div className="hero section center-content illustration-section-01">
-            Start a new Party!
+            {/* Room Doesn't Exist yet */}
             { room === null ?
-                <div style={{ height: '400px' }}>
-                    <Button onClick={() => { setTextOpen(true) }}>
+                <div style={{ height: '100px', paddingTop: '10px' }}>
+                    <Button classes={{
+                        root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                        label: classes.label, // class name, e.g. `classes-nesting-label-x`
+                    }}
+                        onClick={() => { setTextOpen(true) }}>
                         New Party Room
                 </Button>
                 </div>
@@ -57,22 +85,30 @@ const Party = (props) => {
             }
             {
                 textOpen === true &&
-                <TextField
-                    onKeyPress={(ev) => {
-                        if (ev.key === 'Enter') {
-                            // Do code here
-                            generatePartyRoom();
-                            ev.preventDefault();
-                        }
-                    }}
-                    color={'white'}
-                    name={'name'}
-                    onChange={onChangeName}
-                />
+                <div>
+                    <p style={{ color: 'white' }}>Name your party!</p>
+                    <TextField
+                        classes={{
+                            root: classes.root, // class name, e.g. `classes-nesting-root-x`
+                            label: classes.label, // class name, e.g. `classes-nesting-label-x`
+                        }}
+                        onKeyPress={(ev) => {
+                            if (ev.key === 'Enter') {
+                                // Do code here
+                                ev.preventDefault();
+                                generatePartyRoom();
+                            }
+                        }}
+                        name="name"
+                        onChange={onChangeName}
+                    />
+                </div>
             }
+            <Backdrop className={classes.backdrop} open={open}>
+                <CircularProgress color="inherit" />
+            </Backdrop>
         </div>
     )
 }
 
-export default Party;
 
