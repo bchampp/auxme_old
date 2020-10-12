1b2fffa6be19322e242505a2e4879e0c9d3046cb
Brent Champion <brent.champion@queensu.ca>
2020-10-12 19:08:14 -0400

    updating arduino code

--- controller/software/main.ino
+++ controller/software/main.ino
@@ -1,3 +1,7 @@
+// Include Libraries 
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
 #include <Adafruit_NeoPixel.h>
 
 // custom libraries
@@ -7,6 +11,8 @@
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
+
+int mode = 0;
 
 typedef struct color {
   // color object representing RGB values
@@ -31,59 +37,20 @@ typedef struct color {
 #define aux_low 120
 #define aux_high 840
 
+// WiFi Credentials
+char auth[] = "DafemG2lDYKwlMdmLxV6egEtb9oDcx5z";
+char ssid[] = "Tilted Towers 2.4Ghz";
+char pass[] = "wuckfestern";
+
 // init NeoPixel Strip
 Adafruit_NeoPixel strip(led_count, led, NEO_GRB + NEO_KHZ800);
+
+BlynkTimer audioTimer;
 
 /* Function Prototypes */
 int getBrightness(int volume);
 color getColor(int volume, int brightness);
 void printData(int volume, int brightness, color pattern);
-
-void setup() {
-  if(DEBUG){ Serial.begin(9600); }
-
-  // pin assignments
-  pinMode(aux, INPUT);
-  pinMode(pot, INPUT);
-
-  pinMode(strobe, OUTPUT);
-  pinMode(reset, OUTPUT);
-
-  // assert href to default 
-  analogReference(DEFAULT);
-
-  // init LED strip
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-
-  // reset the MSGEQ7
-  digitalWrite(reset, HIGH);
-  digitalWrite(reset, LOW);
-}
- 
-void loop() {
-  // pulse MSGEQ7 strobe 
-  digitalWrite(strobe, HIGH); digitalWrite(strobe, LOW);
-
-  // let output settle from pulse
-  delayMicroseconds(25);
-
-  // map filtered aux signal from 0 to 1000
-  int volume = map(constrain(analogRead(aux), aux_low, aux_high), aux_low, aux_high, 0, 1000); 
-  
-  // get brightness level
-  int brightness = getBrightness(volume);
-
-  // get color pattern
-  color pattern = getColor(volume, brightness);
-
-  // let output settle
-  delayMicroseconds(100); 
-
-  if(DEBUG){
-    printData(volume, brightness, pattern);
-  }
-}
 
 int getBrightness(int volume){
   // Currently not using volume to adjust brightness
@@ -128,3 +95,91 @@ void printData(int volume, int brightness, color pattern){
   Serial.print("% \t");
 }
 
+/* --- BLYNK Callback functions --- */
+
+// Sensitivity
+BLYNK_WRITE(V1){
+  int val = param.asInt(); // Assigning value from pin 1 to variable
+  if(DEBUG){
+    Serial.print("Sensitivity Slider is: ");
+    Serial.println(val);
+  }
+}
+
+// Brightness
+BLYNK_WRITE(V2){
+  int val = param.asInt(); // Assigning value from pin 1 to variable
+  if(DEBUG){
+    Serial.print("Sensitivity Slider is: ");
+    Serial.println(val);
+  }
+}
+
+// Color
+BLYNK_WRITE(V3){
+  r = param[0].asInt();
+  g = param[1].asInt();
+  b = param[2].asInt();
+}
+
+// Mode 
+BLYNK_WRITE(V4){
+  mode = param.asInt();
+}
+
+void audioEvent(){
+// pulse MSGEQ7 strobe 
+  digitalWrite(strobe, HIGH); digitalWrite(strobe, LOW);
+
+  // let output settle from pulse
+  delayMicroseconds(25);
+
+  // map filtered aux signal from 0 to 1000
+  int volume = map(constrain(analogRead(aux), aux_low, aux_high), aux_low, aux_high, 0, 1000); 
+  
+  // get brightness level
+  int brightness = getBrightness(volume);
+
+  // get color pattern
+  color pattern = getColor(volume, brightness);
+ 
+  if(DEBUG){
+    printData(volume, brightness, pattern);
+  }
+
+  Blynk.virtualWrite(V5, volume);
+}
+
+void setup() {
+  if(DEBUG){ Serial.begin(9600); }
+
+  // Init WIFI connection
+  Blynk.begin(auth, ssid, pass);
+  pixels.begin();
+
+  // pin assignments
+  pinMode(aux, INPUT);
+  pinMode(pot, INPUT);
+
+  pinMode(strobe, OUTPUT);
+  pinMode(reset, OUTPUT);
+
+  // assert href to default 
+  analogReference(DEFAULT);
+
+  // init LED strip
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+
+  // reset the MSGEQ7
+  digitalWrite(reset, HIGH);
+  digitalWrite(reset, LOW);
+
+  // Tie timers to callback functions
+  audioTimer.setInterval(200L, audioEvent);
+}
+ 
+void loop() {
+  Blynk.run();
+  audioTimer.run();
+}
